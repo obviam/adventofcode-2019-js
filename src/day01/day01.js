f2a91f4023450f160266db35a520231bda273be9
Impaler <impaler@obviam.net>
2019-12-02 16:34:34 +0000

    Complete day 1

--- src/day01/day01.js
+++ src/day01/day01.js
@@ -4,16 +4,27 @@ const calculateFuelRequired = (mass) => {
     return Math.floor(mass / 3) - 2;
 }
 
-const calculate = (input) => {
-    return input.reduce((acc, curr) => acc + calculateFuelRequired(curr), 0);
+const calculateFuelWithFuel = (mass) => {
+    const fuel = calculateFuelRequired(mass);
+    if (fuel <= 0) return 0; 
+
+    return fuel + calculateFuelWithFuel(fuel);
 }
 
-const result = () => {
-    return calculate(Utils.readInput('day01/input.01.txt'));
+const calculate = (input, calculator) => {
+    return input.reduce((acc, curr) => acc + calculator(curr), 0);
 }
 
-console.log(result());
+const executeAndPrint = () => {
+    const star1 = calculate(Utils.readInput('day01/input.01.txt'), calculateFuelRequired);
+    const star2 = calculate(Utils.readInput('day01/input.01.txt'), calculateFuelWithFuel);
+    console.log('Result star/day 1/01: ' + star1);
+    console.log('Result star/day 2/01: ' + star2);
+}
+
+executeAndPrint();
 
 exports.calculateFuelRequired = calculateFuelRequired;
+exports.calculateFuelWithFuel = calculateFuelWithFuel;
 exports.calculate = calculate;
-// show result
+exports.execute = executeAndPrint();
--- test/day01.test.js
+++ test/day01.test.js
@@ -1,13 +1,16 @@
 const expect = require('chai').expect;
 const calculateFuelRequired = require('../src/day01/day01').calculateFuelRequired;
+const calculateFuelWithFuel = require('../src/day01/day01').calculateFuelWithFuel;
 
-it('Should calculate fuel required', () => {
+it('Should calculate fuel required for mass', () => {
     expect(calculateFuelRequired(12)).to.equal(2);
     expect(calculateFuelRequired(14)).to.equal(2);
     expect(calculateFuelRequired(1969)).to.equal(654);
     expect(calculateFuelRequired(100756)).to.equal(33583);
 })
 
-it('Should execute day 01', () => {
-
+it('Should calculate fuel required for mass with fuel', () => {
+    expect(calculateFuelWithFuel(14, 0)).to.equal(2);
+    expect(calculateFuelWithFuel(1969, 0)).to.equal(966);
+    expect(calculateFuelWithFuel(100756, 0)).to.equal(50346);
 })
