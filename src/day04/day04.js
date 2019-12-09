7d1b6a4d8e16b5394932ac605d8c60c48dd85389
Impaler <impaler@obviam.net>
2019-12-09 12:29:09 +0000

    [wip] day 4 star 2

--- src/day04/day04.js
+++ src/day04/day04.js
@@ -19,6 +19,38 @@ const isValidPassword = (input) => {
     return hasDouble;
 }
 
+const isValidPasswordV2 = (input) => {
+    input = '' + input;
+    if(input.length != 6) {
+        return false;
+    }
+    let hasDouble = false;
+    for (let i = 0; i < input.length - 3; i++) {
+        if (input[i] === input[i + 1] && 
+            input[i] !== input[i - 1] && 
+            input[i] !== input[i + 2]) {
+            hasDouble = true;
+        }
+        if ((Number(input[i]) < Number(input[i - 1])) || (Number(input[i] > Number(input[i + 1])))) {
+            return false;
+        }
+    }
+    
+    if (input[input.length - 3] > input[input.length - 2] || input[input.length - 2] > input[input.length - 1]) {
+        return false;
+    }
+
+    if (!hasDouble) {
+        if (input[input.length - 3] < input[input.length - 2]) {
+            if (input[input.length - 2] === input[input.length - 1]) {
+                return true;
+            }
+        }
+    }
+
+    return hasDouble;
+}
+
 const findValidPasswords = (inputStart, inputEnd) => {
     const passwords = [];
     for (let i = inputStart; i <= inputEnd; i++) {
@@ -26,7 +58,17 @@ const findValidPasswords = (inputStart, inputEnd) => {
             passwords.push(i);
         }
     }
-    console.table(passwords);
+    return passwords.length;
+}
+
+const findValidPasswordsV2 = (inputStart, inputEnd) => {
+    const passwords = [];
+    for (let i = inputStart; i <= inputEnd; i++) {
+        if (isValidPasswordV2(i)) {
+            passwords.push(i);
+        }
+    }
+    // console.table(passwords);
     return passwords.length;
 }
 
@@ -35,12 +77,14 @@ const executeAndPrint = () => {
     console.log('* > Day 4 <');
     console.log('*'.repeat(60));
     const star1 = findValidPasswords(inputStart, inputEnd);
+    const star2 = findValidPasswordsV2(inputStart, inputEnd);
     console.log('* Result star/day 1/04: ' + star1);
-    // console.log('* Result star/day 2/04: ' + star2);
+    console.log('* Result star/day 2/04: ' + star2);
     console.log('*'.repeat(60));
   }
   
 executeAndPrint();
-  
+
 exports.isValidPassword = isValidPassword;
+exports.isValidPasswordV2 = isValidPasswordV2;
 exports.findValidPasswords = findValidPasswords;
--- test/day04.test.js
+++ test/day04.test.js
@@ -1,10 +1,18 @@
 const expect = require('chai').expect;
 const isValidPassword = require('../src/day04/day04').isValidPassword;
+const isValidPasswordV2 = require('../src/day04/day04').isValidPasswordV2;
 
 describe('Day 4', () => {
     it ('should find valid combinations', () => {
         expect(isValidPassword('111111')).to.be.true;
         expect(isValidPassword('223450')).to.be.false;
         expect(isValidPassword('123789')).to.be.false;
+    });
+
+    it ('should find valid combinations for v2', () => {
+        expect(isValidPasswordV2('112233')).to.be.true;
+        expect(isValidPasswordV2('123444')).to.be.false;
+        expect(isValidPasswordV2('111122')).to.be.true;
+        expect(isValidPasswordV2('111222')).to.be.false;
     });
 })
